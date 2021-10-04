9ff829213c0b8aeaed08cc139f73bbabf88ec1c6
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-10-04 17:17:48 -0400

    gitops:0.3.0

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.weaveworks_gitops
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-weaveworks_gitops/actions/workflows/build.yml/badge.svg)
+
+Installs WeaveWorks [gitops](https://github.com/weaveworks/weave-gitops).
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.weaveworks_gitops
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksums.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/weaveworks/weave-gitops/releases/download
+
+# https://github.com/weaveworks/weave-gitops/releases/download/v0.3.0/gitops-linux-x86_64
+
+dl() {
+    local ver=$1
+    local app=$2
+    local lchecksums=$3
+    local os=$4
+    local arch=$5
+    local platform="${os}-${arch}"
+    local file="${app}-${platform}"
+    local url="$MIRROR/v$ver/$file"
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    local app=$2
+    # https://github.com/weaveworks/weave-gitops/releases/download/v0.3.0/gitops_checksums.txt
+    local url="$MIRROR/v$ver/${app}_checksums.txt"
+    local lchecksums="$DIR/${app}_${ver}_checksums.txt"
+    if [ ! -e $lchecksums ];
+    then
+        wget -q -O $lchecksums $url
+    fi
+
+    printf "  # %s\n" $url
+    printf "  '%s':\n" $ver
+
+    dl $ver $app $lchecksums darwin arm64
+    dl $ver $app $lchecksums darwin x86_64
+    dl $ver $app $lchecksums linux arm64
+    dl $ver $app $lchecksums linux x86_64
+}
+
+dl_ver ${1:-0.3.0} gitops
