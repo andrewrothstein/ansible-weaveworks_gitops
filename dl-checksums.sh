8b40f2ae032fb2f57951bc4e40224ebe3189cc1b
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-01-02 20:01:22 -0500

    gitops:0.6.0

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -11,8 +11,9 @@ dl() {
     local lchecksums=$3
     local os=$4
     local arch=$5
+    local archive_type=${6:-tar.gz}
     local platform="${os}-${arch}"
-    local file="${app}-${platform}"
+    local file="${app}-${platform}.${archive_type}"
     local url="$MIRROR/v$ver/$file"
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
@@ -21,7 +22,7 @@ dl() {
 dl_ver() {
     local ver=$1
     local app=$2
-    # https://github.com/weaveworks/weave-gitops/releases/download/v0.3.0/gitops_checksums.txt
+    # https://github.com/weaveworks/weave-gitops/releases/download/v0.6.0/gitops_checksums.txt
     local url="$MIRROR/v$ver/${app}_checksums.txt"
     local lchecksums="$DIR/${app}_${ver}_checksums.txt"
     if [ ! -e $lchecksums ];
@@ -35,7 +36,8 @@ dl_ver() {
     dl $ver $app $lchecksums darwin arm64
     dl $ver $app $lchecksums darwin x86_64
     dl $ver $app $lchecksums linux arm64
+    dl $ver $app $lchecksums linux arm
     dl $ver $app $lchecksums linux x86_64
 }
 
-dl_ver ${1:-0.3.0} gitops
+dl_ver ${1:-0.6.0} gitops
